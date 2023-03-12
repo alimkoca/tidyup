233fbdab670f59435d2bbf39fd8fdae5de792d12
Ali Koca <kinetixcicocuk@gmail.com>
2023-03-12 12:55:46 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(name = "TidyUp", author = "Ali Koca. <kinetixcicocuk@gmail.com>", version = "0.1", about = "Tidying up your folders")]
-struct Args{
+struct Args {
     #[arg(short, long)]
     folder: String,
 }
